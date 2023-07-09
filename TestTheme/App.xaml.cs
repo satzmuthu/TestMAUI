8aa5cd41d2cbbda8d4ebbb5c8d014171b01562d4
Satheesh Kumar T <satzmuthu@outlook.com>
2023-07-09 13:08:23 +0530

    Add files via upload

---
+++ TestTheme/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+
+namespace TestTheme;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+        this.LoadPredefinedTheme();
+		MainPage = new AppShell();
+	}
+
+    private void LoadPredefinedTheme()
+    {
+        var themeName = $"TestTheme.Style.CustomTheme";
+
+        if (Current.Resources.MergedDictionaries.Any(x => x.ToString() == themeName)) return;
+
+        var assembly = IntrospectionExtensions.GetTypeInfo(typeof(App)).Assembly;
+        ResourceDictionary resourceDictionary = null;
+        try
+        {
+            var types = assembly.GetTypes();
+
+            if (types.Any(x => x.FullName != null && x.FullName.Equals(themeName)))
+                resourceDictionary = assembly.CreateInstance(themeName) as ResourceDictionary;
+
+            if (resourceDictionary != null)
+            {
+                Current.Resources.MergedDictionaries.Add(resourceDictionary);
+            }
+        }
+        catch (Exception)
+        {
+            //Ignore and pass null to get default theme
+        }
+    }
+}
---
+++ TestTheme/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TestTheme;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
